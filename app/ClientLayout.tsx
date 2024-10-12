6064220f6471c1fb150e5fb8b4b133f45ecd12e4
unknown <marianolark@gmail.com>
2024-10-12 13:08:39 +0800

    SUN RAY

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -15,7 +15,7 @@ const ClientLayout: React.FC<ClientLayoutProps> = ({ children }) => {
   const isHomePage = pathname === '/';
 
   // Array of different left positions
-  const positions = [1, 10, 25, 40, 50, 60, 70, 90];
+  const positions = [1, 20 , 37,  65,  80];
 
   return (
     <div className={`bg-bg-img-6 bg-no-repeat bg-cover bg-center ${isHomePage ? 'lg:h-[1240px] h-[800px]' : 'lg:h-[auto] h-[auto]'}`}>
@@ -23,20 +23,22 @@ const ClientLayout: React.FC<ClientLayoutProps> = ({ children }) => {
       {positions.map((pos, index) => (
         <motion.div
           key={index}
-          className="absolute top-[-5%] -translate-x-1/2 h-full bg-gradient-to-b from-white to-transparent opacity-20 z-[10000]"
+          className="absolute top-[-5%] -translate-x-1/2 h-[90%] lg:h-[120%] bg-gradient-to-b from-[#ffffff] to-transparent opacity-20 z-[10000] white__gradient"
           style={{
             width: '200px',
-            clipPath: 'polygon(0% 0%, 15% 0%, 50% 100%, 45% 100%)',
+            clipPath: 'polygon(0% 0%, 40% 0%, 50% 100%, 45% 100%)',
             transform: 'rotate(30deg)',
-            left: `${pos}%` // Randomize the left position
+            left: `${pos}%`, // Randomize the left position
+            filter: 'blur(100px)' // Optional blur effect
+
           }}
           animate={{
             opacity: [0, 0.5, 0], // Fade in and fade out
           }}
           transition={{
-            duration: 2, // Duration of the fade
+            duration: 4, // Duration of the fade
             repeat: Infinity, // Infinite loop
-            delay: Math.random() * 2, // Random delay for each ray to desynchronize
+            delay: Math.random() * 4, // Random delay for each ray to desynchronize
           }}
         ></motion.div>
       ))}
