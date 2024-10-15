9e7c81862d7c1ab4b610f83340bf478beae755a3
unknown <marianolark@gmail.com>
2024-10-15 20:11:24 +0800

    LATEST

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Kraken</title>
+        <meta name="description" content="Kraken Portfolio" />
+
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Kraken Portfolio" />
+        <meta property="og:description" content="Explore the Kraken portfolio showcasing amazing projects and accomplishments." />
+        <meta property="og:image" content="/krakenlogo.svg" /> {/* Make sure this image is accessible from public folder */}
+        <meta property="og:type" content="website" />
+        {/* Additional social sharing meta tags */}
+        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kraken Portfolio" />
+        <meta name="twitter:description" content="Explore the Kraken portfolio showcasing amazing projects and accomplishments." />
+        <meta name="twitter:image" content="/kraken-og-image.png" /> */}
+        <link rel="icon" href="/krakenlogo.svg" />
+      </head>
       <body>
-      <ClientLayout>{children}</ClientLayout>
-      <link rel="icon" href="/krakenlogo.svg" />
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
